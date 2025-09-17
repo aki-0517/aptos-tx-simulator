8059296024d797ac2a0c427e285e9b1680fe6ca5
aki-0517 <akki.pp.777@gmail.com>
2025-09-17 10:46:39 +0900

    change header

--- web/src/components/common/Header.tsx
+++ web/src/components/common/Header.tsx
@@ -25,9 +25,6 @@ export function Header() {
         {/* Logo & Title */}
         <div className="flex items-center space-x-3">
           <Link href="/" className="flex items-center space-x-2">
-            <div className="w-6 h-6 bg-primary rounded flex items-center justify-center">
-              <span className="text-primary-foreground font-bold text-xs">A</span>
-            </div>
             <span className="font-medium text-sm">Aptos Transaction Simulator</span>
           </Link>
         </div>
